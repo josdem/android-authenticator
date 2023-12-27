cae0bc5c71e1a3cad9275d4713d71b09907369af
josdem <joseluis.delacruz@gmail.com>
2023-12-27 14:16:41 -0500

    #1 Adding Retrofit get token request

--- app/src/main/java/com/josdem/authenticator/service/AuthService.kt
+++ app/src/main/java/com/josdem/authenticator/service/AuthService.kt
@@ -2,11 +2,18 @@ package com.josdem.authenticator.service
 
 import com.josdem.authenticator.data.model.AccessTokenResponse
 import retrofit2.Response
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
 import retrofit2.http.POST
 
 interface AuthService {
 
     @POST("oauth2/token")
-    suspend fun getToken(): Response<AccessTokenResponse>
+    @FormUrlEncoded
+    suspend fun getToken(
+        @Field("clientId") clientId: String,
+        @Field("clientSecret") clientSecret: String,
+        @Field("scope") scope: String
+    ): Response<AccessTokenResponse>
 
 }
