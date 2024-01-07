002ad98622ae785c81da76ea24345c02498d6079
josdem <joseluis.delacruz@gmail.com>
2024-01-07 15:40:41 -0500

    #7 Updating Gradle and Kotlin versions

--- app/src/test/java/com/josdem/authenticator/LoginRepositoryTest.kt
+++ app/src/test/java/com/josdem/authenticator/LoginRepositoryTest.kt
@@ -1,11 +1,40 @@
 package com.josdem.authenticator
 
+import android.util.Log
+import com.josdem.authenticator.data.LoginDataSource
+import com.josdem.authenticator.data.LoginRepository
+import com.josdem.authenticator.data.Result
+import com.josdem.authenticator.data.model.AccessTokenResponse
+import io.mockk.MockKAnnotations
+import io.mockk.every
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.MockK
+import io.mockk.mockkStatic
 import org.junit.Assert.assertEquals
+import org.junit.Before
 import org.junit.Test
 
 class LoginRepositoryTest {
+
+    @MockK
+    private lateinit var dataSource: LoginDataSource
+    
+    private val accessTokenResponse = AccessTokenResponse("token", "access", 100, "scope")
+    private val result = Result.Success(accessTokenResponse)
+
+    @Before
+    fun setUp() = MockKAnnotations.init(this)
+
     @Test
     fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+        mockkStatic(Log::class)
+        every { Log.d(any(), any()) } returns 0
+        every { dataSource.login("josdem", "password") } returns result
+
+        var loginRepository = LoginRepository(dataSource)
+
+        val login = loginRepository.login("josdem", "password")
+
+        assertEquals(result, login)
     }
 }
