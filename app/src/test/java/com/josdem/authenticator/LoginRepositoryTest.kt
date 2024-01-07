413070907ca0aee6daa0cc81e790669f58dc6123
josdem <joseluis.delacruz@gmail.com>
2024-01-07 15:10:08 -0500

    #7 Adding mockk dependency

---
+++ app/src/test/java/com/josdem/authenticator/LoginRepositoryTest.kt
@@ -0,0 +1,11 @@
+package com.josdem.authenticator
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class LoginRepositoryTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
