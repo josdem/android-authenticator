cb6e39f30fcad2216fe0becc09a3b81315f59628
josdem <joseluis.delacruz@gmail.com>
2024-03-01 08:11:08 -0500

    #5 Adding get message instance

--- app/src/main/java/com/josdem/authenticator/service/MessageService.kt
+++ app/src/main/java/com/josdem/authenticator/service/MessageService.kt
@@ -1,15 +1,14 @@
 package com.josdem.authenticator.service
 
 import retrofit2.Call
-import retrofit2.http.Field
+import retrofit2.http.Body
 import retrofit2.http.Header
 import retrofit2.http.POST
 
 interface MessageService {
-
     @POST("/message")
     fun sendMessage(
         @Header("Authorization") authorization: String,
-        @Field("data") scope: String,
+        @Body message: String,
     ): Call<String>
 }
