835a5c400c59b635db9171e7ae27e2e1b7e9eb1f
josdem <joseluis.delacruz@gmail.com>
2024-03-01 08:34:09 -0500

    #5 Adding get message data source

---
+++ app/src/main/java/com/josdem/authenticator/data/MessageDataSource.kt
@@ -0,0 +1,24 @@
+package com.josdem.authenticator.data
+
+import android.util.Log
+import com.josdem.authenticator.service.MessageService
+import com.josdem.authenticator.service.RetrofitHelper
+import com.josdem.authenticator.state.ApplicationState
+import java.io.IOException
+
+class MessageDataSource {
+    val messageService = RetrofitHelper.getInstance().create(MessageService::class.java)
+
+    fun sendMessage(message: String): String {
+        val call =
+            messageService.sendMessage("Bearer " + ApplicationState.getValue("token"), message)
+        return try {
+            val result = call.execute()
+            Log.d("response: ", result.body().toString())
+            result.body().toString()
+        } catch (ioe: IOException) {
+            Log.d("Exception in sending message: ", ioe.message.toString())
+            ioe.message.toString()
+        }
+    }
+}
