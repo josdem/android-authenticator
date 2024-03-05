8713d4411c901e2b2cc5d916cf1c457c78c1ad68
josdem <joseluis.delacruz@gmail.com>
2024-03-05 17:06:40 -0500

    #6 Adding get message request

--- app/src/main/java/com/josdem/authenticator/service/MessageService.kt
+++ app/src/main/java/com/josdem/authenticator/service/MessageService.kt
@@ -2,6 +2,7 @@ package com.josdem.authenticator.service
 
 import retrofit2.Call
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.POST
 
@@ -10,5 +11,10 @@ interface MessageService {
     fun sendMessage(
         @Header("Authorization") authorization: String,
         @Body message: String,
+    ): Call<String>
+
+    @GET("/message")
+    fun getMessage(
+        @Header("Authorization") authorization: String,
     ): Call<String>
 }
