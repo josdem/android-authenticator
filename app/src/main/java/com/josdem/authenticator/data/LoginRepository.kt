26730447f9bf84dcbcb3f9cc8fe4bb89b9524b5b
josdem <joseluis.delacruz@gmail.com>
2024-03-09 15:18:11 -0500

    #18 Adding license to data

--- app/src/main/java/com/josdem/authenticator/data/LoginRepository.kt
+++ app/src/main/java/com/josdem/authenticator/data/LoginRepository.kt
@@ -1,3 +1,20 @@
+/*
+Copyright 2024 Jose Morales contact@josdem.io
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, either express or implied.  See the License for the
+specific language governing permissions and limitations
+under the License.
+ */
+
 package com.josdem.authenticator.data
 
 import android.util.Log
@@ -9,7 +26,6 @@ import com.josdem.authenticator.data.model.AccessTokenResponse
  */
 
 class LoginRepository(val dataSource: LoginDataSource) {
-    // in-memory cache of the loggedInUser object
     var user: AccessTokenResponse? = null
         private set
 
@@ -31,7 +47,6 @@ class LoginRepository(val dataSource: LoginDataSource) {
         username: String,
         password: String,
     ): Result<AccessTokenResponse> {
-        // handle login
         val result = dataSource.login(username, password)
 
         Log.d("credentials: ", "$username:$password")
